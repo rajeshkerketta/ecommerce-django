994ecd380da3ca7bc76465a221457f96b155687e
Rajesh Kerketta <257819504+rajeshkerketta@users.noreply.github.com>
2026-04-10 02:02:41 +0530

    serve media on render

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -1,7 +1,3 @@
-"""
-URL configuration for ecommerce project.
-"""
-
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -9,15 +5,10 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
     path('', include('products.urls')),
     path('cart/', include('cart.urls')),
-
     path('accounts/', include('django.contrib.auth.urls')),
-    
     path('accounts/', include('accounts.urls')),
-    # path('accounts/', include('allauth.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
